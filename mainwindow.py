ed88271966ee59d703f42a07cc2a96127506d1ce
Michael Wierlemann <mica.wierlemann@gmail.com>
2022-04-22 02:42:23 +0200

    Element event handling, additional elements

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QPushButton, QLineEdit, QComboBox, QFrame;
 from PyQt5.QtGui import QFont;
+from PyQt5.QtCore import Qt;
 
 appVersion: str = "v.1.0";
 
@@ -67,6 +68,7 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.PlotTypeBox, 0, 1);
 
         self.PlotTypeOkBtn = QPushButton("OK");
+        self.PlotTypeOkBtn.clicked.connect(self.plotTypeBtnClicked);
         self.Grid.addWidget(self.PlotTypeOkBtn, 0, 2);
 
         self.SelectYLbl = QLabel();
@@ -74,11 +76,12 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.SelectYLbl, 2, 0);
 
         self.SelectYBox = QComboBox();
-        self.SelectYBox.addItem("Insert x-data manually");
+        self.SelectYBox.addItem("Insert data manually");
         self.SelectYBox.addItem("Load data from file");
         self.Grid.addWidget(self.SelectYBox, 2, 1);
 
         self.OkYBtn = QPushButton("OK");
+        self.OkYBtn.clicked.connect(self.yOkBtnClicked);
         self.Grid.addWidget(self.OkYBtn, 2, 2);
 
         self.YDataLine = QLineEdit();
@@ -86,7 +89,9 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.YDataLine, 3, 0, 1, 4);
 
         self.LoadedYFileLbl = QLabel();
-        self.Grid.addWidget(self.LoadedYFileLbl, 3, 0);
+        self.LoadedYFileLbl.setText("No file loaded");
+        self.LoadedYFileLbl.setFixedHeight(16);
+        self.Grid.addWidget(self.LoadedYFileLbl, 3, 0, 1, 4);
         self.LoadedYFileLbl.hide();
 
         self.SelectXLbl = QLabel();
@@ -99,6 +104,7 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.SelectXBox, 5, 1);
 
         self.OkXBtn = QPushButton("OK");
+        self.OkXBtn.clicked.connect(self.plotTypeBtnClicked);
         self.Grid.addWidget(self.OkXBtn, 5, 2);
 
         self.XDataLine = QLineEdit();
@@ -106,12 +112,21 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.XDataLine, 6, 0, 1, 4);
 
         self.LoadedXFileLbl = QLabel();
+        self.LoadedXFileLbl.setText("No file loaded");
+        self.LoadedXFileLbl.setFixedHeight(28);
         self.Grid.addWidget(self.LoadedXFileLbl, 6, 0);
         self.LoadedXFileLbl.hide();
 
+        self.XBinsLbl = QLabel();
+        self.XBinsLbl.setText("No. of bins: ");
+        self.XBinsLbl.setAlignment(Qt.AlignRight);
+        self.Grid.addWidget(self.XBinsLbl, 6, 0);
+        self.XBinsLbl.hide();
+
         self.XBinsLine = QLineEdit();
-        self.XBinsLine.setPlaceholderText("Type bin separation interval here...");
-        self.Grid.addWidget(self.XBinsLine, 6, 0);
+        self.XBinsLine.setMaxLength(2);
+        self.XBinsLine.setMaximumWidth(20);
+        self.Grid.addWidget(self.XBinsLine, 6, 1);
         self.XBinsLine.hide();
 
         self.PlotBtn = QPushButton("PLOT DATA");
@@ -119,3 +134,37 @@ class MainWindow(QWidget):
         self.Grid.addWidget(self.PlotBtn, 8, 0, 1, 4);
 
         self.show();
+
+    # Ensuring correct elements displayed
+    def plotTypeBtnClicked(self):
+        currPlot: str = self.PlotTypeBox.currentText();
+        currXMethod: str = self.SelectXBox.currentText();
+        if(((currPlot == "Standard Plot") | (currPlot == "Logarithmic Plot")) & (currXMethod == "Insert data manually")):
+            self.XDataLine.show();
+            self.XBinsLine.hide();
+            self.XBinsLbl.hide();
+            self.LoadedXFileLbl.hide(); 
+        elif(((currPlot == "Standard Plot") | (currPlot == "Logarithmic Plot")) & (currXMethod == "Load data from file")):
+            self.XDataLine.hide();
+            self.XBinsLine.hide();
+            self.XBinsLbl.hide();
+            self.LoadedXFileLbl.show();
+        elif((currPlot == "Histogram") & (currXMethod == "Insert data manually")):
+            self.XDataLine.hide();
+            self.XBinsLine.show();
+            self.XBinsLbl.show();
+            self.LoadedXFileLbl.hide();
+        elif((currPlot == "Histogram") & (currXMethod == "Load data from file")):
+            self.XDataLine.hide();
+            self.XBinsLine.hide();
+            self.XBinsLbl.hide();
+            self.LoadedXFileLbl.show();
+    
+    def yOkBtnClicked(self):
+        currYMethod: str = self.SelectYBox.currentText();
+        if(currYMethod == "Insert data manually"):
+            self.YDataLine.show();
+            self.LoadedYFileLbl.hide();
+        else:
+            self.YDataLine.hide();
+            self.LoadedYFileLbl.show();
